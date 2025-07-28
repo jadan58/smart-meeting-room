06490fc76161b93175326cdfb7317439bcc52eee
Jad Abou Najem <jadabonajem5@gmail.com>
2025-07-28 11:58:43 +0300

    Set up Room module with repository pattern, DTOs, AutoMapper, and  controller

---
+++ Repositories/IRoomRepository.cs
@@ -0,0 +1,16 @@
+﻿using SmartMeetingRoomAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SmartMeetingRoomAPI.Repositories
+{
+    public interface IRoomRepository
+    {
+        Task<IEnumerable<Room>> GetAllAsync();
+        Task<Room?> GetByIdAsync(Guid id);
+        Task<Room> AddAsync(Room room);
+        Task<Room?> UpdateAsync(Guid id, Room room);
+        Task<Room?> DeleteAsync(Guid id);
+    }
+}
