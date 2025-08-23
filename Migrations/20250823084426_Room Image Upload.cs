5a05337ceaac250cca5d1d13b513b7db3c4cd2b8
jadan58 <jadabonajem5@gmail.com>
2025-08-23 12:17:10 +0300

    final image upload

---
+++ Migrations/20250823084426_Room Image Upload.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartMeetingRoomAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoomImageUpload : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Rooms",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Rooms");
+        }
+    }
+}
